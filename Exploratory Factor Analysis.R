f3621df9199d4d01e3751424821f0663ec1f12a4
Rafael Valdece Sousa Bastos <rafavsbastos@gmail.com>
2020-08-12 15:07:26 -0300

    Update Exploratory Factor Analysis.R

--- Exploratory Factor Analysis.R
+++ Exploratory Factor Analysis.R
@@ -63,9 +63,6 @@ print(fit, sort = TRUE)
 fa.diagram(fit)
 summary(fit, fit.measures=TRUE, standardized=TRUE)
 
-#Modification indices function goes as follows
-modificationIndices(fit, standardized = TRUE, cov.std = TRUE)
-
 --------
 ###Calculating reliability with Alpha and Omega
   #Select the items you want to use to calculate the reliability by putting each name in paranthesis
