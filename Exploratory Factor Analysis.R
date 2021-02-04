3149b87a8a6a2027986a5a61a28e3132d4c9ba92
Rafael Valdece Sousa Bastos <rafavsbastos@gmail.com>
2021-02-04 20:41:42 -0300

    Update Exploratory Factor Analysis.R

--- Exploratory Factor Analysis.R
+++ Exploratory Factor Analysis.R
@@ -20,42 +20,45 @@ View(DemoData)
 variables <- DemoData[,c("item1", "item2", "item3", "item4")]
 
 ### Exploratory Factor Analysis
-    
+
+#Do a polychoric correlation
+cor_data <- polychoric(x = variables, smooth = TRUE, global = TRUE, 
+                       polycor = FALSE, ML = FALSE, std.err = FALSE,
+                       weight = NULL, progress = TRUE, na.rm = TRUE,
+                       delete = TRUE)
+poly_cor <- cor_data$rho
 ###Testing the quality of the data with KMO and Bartlett
 
-KMO(variables)
+KMO(poly_cor)
 
 #In the next line: n = number of cases in integer
-cortest.bartlett(variables, n=120,diag = TRUE)
+cortest.bartlett(poly_cor, n=120,diag = TRUE)
 
 # Calculate split-half reliability
-splitHalf(variables)
-
-#The correlation matrix of the items
-lowerCor(variables)
+splitHalf(poly_cor)
 
 ### Parallel Analysis
-  #fm = extraction method; I'm using maximum likelihood, but you can use another one
-    #for other methods, please code help(package="psych") and search for fa.parallel
-  #fa: if you want to use principal componentes use fa = "pc"; fa = "fa" use a principal axis factor analysis
+#fm = extraction method; I'm using maximum likelihood, but you can use another one
+#for other methods, please code help(package="psych") and search for fa.parallel
+#fa: if you want to use principal componentes use fa = "pc"; fa = "fa" use a principal axis factor analysis
 
-fa.parallel(variables, n.obs = NULL,fm="ml", fa="fa",
+fa.parallel(poly_cor, n.obs = NULL,fm="ml", fa="fa",
             main = "Parallel Analysis Scree Plots", n.iter=500,error.bars=FALSE,
             SMC=FALSE,ylabel=NULL,show.legend=TRUE,sim=TRUE)
 
 
 #You can use paran packege too, it shows the value of simulated eigenvalues
-paran(variables, iterations=500, centile=0, quietly=FALSE, 
+paran(mat = poly_cor, n=120, iterations=500, centile=0, quietly=FALSE, 
       status=TRUE, all=TRUE, cfa=TRUE, graph=TRUE, 
       color=TRUE, col=c("black","red","blue"), 
       lty=c(1,2,3), lwd=1, legend=TRUE, file="", 
-      width=640, height=640, grdevice="png", seed=0, mat=NA, n=NA)
+      width=640, height=640, grdevice="png", seed=0)
 
 ### Factor analysis: fit indices
 
-  #set the number of factors in "nfactors" and the number of observation in "n.obs"
-  #rotation methods and extraction methods vary, use help(package = "psych") and go to "fa"
-fit <- fa(variables, nfactors = 1, n.obs = 120, rotate = "oblimin", residuals = TRUE, cor = "poly", fm = "minres")
+#set the number of factors in "nfactors" and the number of observation in "n.obs"
+#rotation methods and extraction methods vary, use help(package = "psych") and go to "fa"
+fit <- fa(poly_cor, nfactors = 1, n.obs = 120, rotate = "oblimin", residuals = TRUE, cor = "poly", fm = "minres")
 print(fit, sort = TRUE)
 
 fa.diagram(fit)
