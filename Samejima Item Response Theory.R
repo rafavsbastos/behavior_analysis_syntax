49a0abfc2f4d2c6d9124f471c92a1b59c5691fc4
Rafael Valdece Sousa Bastos <rafavsbastos@gmail.com>
2021-02-04 20:25:09 -0300

    Update Samejima Item Response Theory.R

--- Samejima Item Response Theory.R
+++ Samejima Item Response Theory.R
@@ -16,7 +16,9 @@ View(DemoData)
 # I'm using the model with one factor (that's why the integer 1)
 
 model <- mirt(DemoData, 1, itemtype = 'graded')
-summary(model)
+factor_loadings <- summary(model)
+factor loadings
+
 itemfit(model)
 coefmod1<-coef(model, IRTpars=TRUE, simplify = TRUE)
 coefmod1
@@ -38,3 +40,33 @@ itemplot(model,1, type="trace", main="CCI item1")
 itemplot(model,2, type="trace", main="CCI item2")
 itemplot(model,3, type="trace", main="CCI item3")
 itemplot(model,4, type="trace", main="CCI item4")
+
+#Lets make a csv table with our data
+
+factor_table <- data.frame(factor_loadings, row.names = NULL)
+r_factor_table <- round(factor_table, 3)
+
+table <- round(coefmod1$items, 3)
+inf_table <- data.frame(table, row.names = NULL)
+inf_table
+
+#Change nrow for the number of items in your scale
+table <- matrix(NA, nrow = 4, ncol = 10)
+
+#Write the label of the items bellow; just follow the pattern
+table[,1] <- c("Item 1", "Item 2", "Item 3", "Item 4")
+
+table[,2] <- r_factor_table[,1]
+table[,3] <- r_factor_table[,2]
+table[,4] <- inf_table[,1]
+table[,5] <- inf_table[,2]
+table[,6] <- inf_table[,3]
+table[,7] <- inf_table[,4]
+table[,8] <- inf_table[,5]
+table[,9] <- inf_table[,6]
+table[,10] <- inf_table[,7]
+table
+
+#The file will be saved where this code is saved
+write.csv2(table, file = "IRT.csv", sep = ",", quote = FALSE, 
+            row.names = FALSE)
